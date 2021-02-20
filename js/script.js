776c1913ded099174267ca0c72544fa4a5a778c5
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2021-02-20 13:37:53 +0300

    hw3

--- js/script.js
+++ js/script.js
@@ -12,3 +12,43 @@ const question3 = {
 const question4 = {
 
 }
+
+console.log('Задание 1; a = 1');
+
+let a = 1;
+
+if (a === 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+console.log('Задание 1; a = 0');
+
+a = 0;
+
+if (a === 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+console.log('Задание 1; a = -3');
+
+a = -3;
+
+if (a === 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+console.log('Задание 2');
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i, 'Переменная четная');
+    } else {
+        console.log(i, 'Переменная нечетная');
+    }
+}
