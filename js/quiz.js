70b16312a229b665301e52140d4f69330c7af85b
Evgeniya <evgeniya_ignaten@mail.ru>
2021-06-08 17:27:46 +0300

    hw12

--- js/quiz.js
+++ js/quiz.js
@@ -1,43 +1,62 @@
-
+// const quizWrap = document.querySelector('.quiz-wrap');
+// const form = document.querySelector('.form');
+// let error = document.createElement('div');
+// error.className = 'error-block';
+//
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+let quizWrap = document.querySelector('.quiz-wrap');
+let form = document.querySelector('.form');
+let name = document.querySelector('.name');
+let error = document.createElement('div');
+error.className = 'error-block';
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const restartButton = document.getElementById("restart");
+let currentSlide = 0;
+let questions = [];
+let slides;
 
 class Quiz {
-  constructor() {
-    this.quizContainer = document.getElementById('quiz');
-    this.resultsContainer = document.getElementById('results');
-    this.submitButton = document.getElementById('submit');
-    this.quiz = document.querySelector('.quiz-wrap');
-    this.form = document.querySelector('.form');
-    this.name = document.querySelector('.name');
-    this.error = document.createElement('div');
-    this.error.className = 'error-block';
-    this.previousButton = document.getElementById("previous");
-    this.nextButton = document.getElementById("next");
-    this.restartButton = document.getElementById("restart");
-    this.currentSlide = 0;
-    this.questions = [];
-  }
+  // constructor() {
+  //   this.quizContainer = document.getElementById('quiz');
+  //   this.resultsContainer = document.getElementById('results');
+  //   this.submitButton = document.getElementById('submit');
+  //   // this.quizWrap = document.querySelector('.quiz-wrap');
+  //   // this.form = document.querySelector('.form');
+  //   this.name = document.querySelector('.name');
+  //   // this.error = document.createElement('div');
+  //   // this.error.className = 'error-block';
+  //   this.previousButton = document.getElementById("previous");
+  //   this.nextButton = document.getElementById("next");
+  //   this.restartButton = document.getElementById("restart");
+  //   this.currentSlide = 0;
+  //   this.questions = [];
+  // }
 
   validation(event) {
+    console.log(error);
     let regex = /^[А-ЯЁ][а-яё]{1,9}$/;
-    this.name.classList.remove('error');
+    name.classList.remove('error');
 
-    if(!regex.test(this.name.value)) {
+    if(!regex.test(name.value)) {
       event.preventDefault();
-      this.name.classList.add('error');
-      this.error.innerHTML = 'Please enter correct name';
-      this.name.parentElement.insertBefore(this.error, this.name);
+      name.classList.add('error');
+      error.innerHTML = 'Please enter correct name';
+      name.parentElement.insertBefore(error, name);
     }
     else {
       event.preventDefault();
-      this.form.style.display = 'none';
-      this.quiz.style.display = 'block';
+      form.style.display = 'none';
+      quizWrap.style.display = 'block';
     }
   }
 
   buildQuiz(){
     const output = [];
 
-    this.questions.forEach(
+    questions.forEach(
       (currentQuestion, questionNumber) => {
         const answers = [];
 
@@ -61,39 +80,42 @@ class Quiz {
       }
     );
 
-    this.quizContainer.innerHTML = output.join('');
+    quizContainer.innerHTML = output.join('');
+    slides = document.querySelectorAll(".slide");
+    console.log(slides);
   }
 
   showSlide(n) {
-    const slides = document.querySelectorAll(".slide");
+    console.log(slides);
     slides[currentSlide].classList.remove('active-slide');
     slides[n].classList.add('active-slide');
-    let currentSlide = n;
+    currentSlide = n;
     if(currentSlide === 0){
-      this.previousButton.style.display = 'none';
+      previousButton.style.display = 'none';
     }
     else{
-      this.previousButton.style.display = 'inline-block';
+      previousButton.style.display = 'inline-block';
     }
     if(currentSlide === slides.length - 1){
-      this.nextButton.style.display = 'none';
+      nextButton.style.display = 'none';
       if (!resultsContainer.innerHTML) {
         submitButton.style.display = 'inline-block';
       }
     }
     else{
-      this.nextButton.style.display = 'inline-block';
-      this.submitButton.style.display = 'none';
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
     }
+    this.setAnswerHandlers();
     // timer()
   }
 
   showNextSlide() {
-    showSlide(currentSlide + 1);
+    this.showSlide(currentSlide + 1);
   }
 
   showPreviousSlide() {
-    showSlide(currentSlide - 1);
+    this.showSlide(currentSlide - 1);
   }
 
   checkResult (e) {
@@ -101,7 +123,7 @@ class Quiz {
     if(tar.tagName === 'INPUT') {
       const questionNumber = tar.name.slice(-1);
       const userAnswer = tar.value;
-      const isCorrect = this.questions[questionNumber].correctAnswer == userAnswer;
+      const isCorrect = questions[questionNumber].correctAnswer == userAnswer;
       if(isCorrect) {
         tar.parentNode.style.color = 'limegreen';
       } else {
@@ -113,16 +135,16 @@ class Quiz {
   }
 
   setAnswerHandler() {
-    Array.from(this.quizContainer.querySelectorAll('.slide .answers')).forEach(answer => {
-      answer.addEventListener('click', checkResult);
+    Array.from(quizContainer.querySelectorAll('.slide .answers')).forEach(answer => {
+      answer.addEventListener('click', this.checkResult);
     })
   }
 
   showResults() {
-    const answerContainers = this.quizContainer.querySelectorAll('.answers');
+    const answerContainers = quizContainer.querySelectorAll('.answers');
     let numCorrect = 0;
 
-    this.questions.forEach((currentQuestion, questionNumber) => {
+    questions.forEach((currentQuestion, questionNumber) => {
 
       const answerContainer = answerContainers[questionNumber];
       const selector = `input[name=question${questionNumber}]:checked`;
@@ -134,9 +156,9 @@ class Quiz {
       }
     });
 
-    this.resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
-    this.submitButton.style.display = 'none';
-    this.restartButton.style.display = 'inline-block';
+    resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+    submitButton.style.display = 'none';
+    restartButton.style.display = 'inline-block';
   }
 
   restart() {
@@ -148,6 +170,8 @@ async function getQuestions() {
   const response = await fetch('https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple');
   const data = await response.json();
   let quiz = new Quiz;
+
+  // console.log( quiz.showSlide());
 
   data.results.forEach(item => {
     let question = item.question;
@@ -172,211 +196,24 @@ async function getQuestions() {
 
     questionForm.correctAnswer = correctNumber;
 
-    quiz.questions.push(questionForm);
+    questions.push(questionForm);
   });
+  console.log(questions);
 
-    quiz.buildQuiz(quiz.questions, quiz.quizContainer);
-    quiz.nextButton.addEventListener('click', () => {
-      quiz.showNextSlide(this.currentSlide);
-    });
-  quiz.previousButton.addEventListener('click', () => {
-      quiz.showPreviousSlide(this.currentSlide);
-    });
+  //   // quiz.buildQuiz(quiz.questions, quiz.quizContainer);
+  //   quiz.buildQuiz();
+  //   quiz.nextButton.addEventListener('click', () => {
+  //     quiz.showNextSlide(this.currentSlide);
+  //   });
+  // quiz.previousButton.addEventListener('click', () => {
+  //     quiz.showPreviousSlide(this.currentSlide);
+  //   });
+  quiz.buildQuiz();
+  form.addEventListener('submit', quiz.validation);
+  submitButton.addEventListener('click', quiz.showResults);
+  previousButton.addEventListener("click", quiz.showPreviousSlide);
+  nextButton.addEventListener("click", quiz.showNextSlide);
+  restartButton.addEventListener("click", quiz.restart);
 }
 
 getQuestions();
-//
-// fetch('https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple')
-//   .then(res => res.json())
-//   .then(json => {
-//     json.results.forEach(item => {
-//       let question = item.question;
-//       let correctAnswer = item.correct_answer;
-//       let answers = item.incorrect_answers;
-//       let questionForm = {};
-//       questionForm.question = question;
-//       questionForm.answers = {};
-//       answers.forEach((currentAnswer, i) => {
-//         questionForm.answers[i] = currentAnswer;
-//       });
-//
-//       let correctNumber = Math.floor(Math.random() * 4);
-//
-//       let temp = questionForm.answers[correctNumber];
-//
-//       questionForm.answers[correctNumber] = correctAnswer;
-//       if (temp !== undefined) {
-//         questionForm.answers[4] = temp;
-//       }
-//
-//       questionForm.correctAnswer = correctNumber;
-//
-//       questions.push(questionForm);
-//     });
-//
-//     buildQuiz();
-//     const slides = document.querySelectorAll(".slide");
-//
-//     function validation(event) {
-//       let regex = /^[А-ЯЁ][а-яё]{1,9}$/;
-//       name.classList.remove('error');
-//
-//       if(!regex.test(name.value)) {
-//         event.preventDefault();
-//         name.classList.add('error');
-//         error.innerHTML = 'Please enter correct name';
-//         name.parentElement.insertBefore(error, name);
-//       }
-//       else {
-//         event.preventDefault();
-//         form.style.display = 'none';
-//         quiz.style.display = 'block';
-//       }
-//     }
-//
-//     function buildQuiz(){
-//       const output = [];
-//
-//       questions.forEach(
-//         (currentQuestion, questionNumber) => {
-//           const answers = [];
-//
-//           for(number in currentQuestion.answers){
-//             answers.push(
-//               `<li>
-//               <label>
-//                 <input type="radio" name="question${questionNumber}" value="${number}">
-//                   ${currentQuestion.answers[number]}
-//               </label>
-//              </li>`
-//             );
-//           }
-//
-//           output.push(
-//             `<div class="slide">
-//             <div class="question"> ${currentQuestion.question} </div>
-//             <ol class="answers"> ${answers.join("")} </ol>
-//           </div>`
-//           );
-//         }
-//       );
-//
-//       quizContainer.innerHTML = output.join('');
-//     }
-//
-//     function timer() {
-//       const slides = document.querySelectorAll(".slide");
-//       const radioBtns = slides[currentSlide].querySelectorAll('INPUT');
-//       let seconds = 10;
-//       let idInt = function() {
-//         if (seconds >= 0) {
-//           seconds--;
-//         } else {
-//           radioBtns.forEach(button => button.setAttribute('disabled', true));
-//           clearInterval(timerId);
-//           if (currentSlide === slides.length - 1) {
-//             showResults();
-//           } else {
-//             showNextSlide();
-//           }
-//         }
-//       };
-//       let timerId = setInterval(idInt, 1000);
-//
-//       nextButton.addEventListener('click', function() {
-//         clearInterval(timerId);
-//       });
-//       previousButton.addEventListener('click', function() {
-//         clearInterval(timerId);
-//       });
-//     }
-//
-//     function showSlide(n) {
-//       const slides = document.querySelectorAll(".slide");
-//       slides[currentSlide].classList.remove('active-slide');
-//       slides[n].classList.add('active-slide');
-//       currentSlide = n;
-//       if(currentSlide === 0){
-//         previousButton.style.display = 'none';
-//       }
-//       else{
-//         previousButton.style.display = 'inline-block';
-//       }
-//       if(currentSlide === slides.length - 1){
-//         nextButton.style.display = 'none';
-//         if (!resultsContainer.innerHTML) {
-//           submitButton.style.display = 'inline-block';
-//         }
-//       }
-//       else{
-//         nextButton.style.display = 'inline-block';
-//         submitButton.style.display = 'none';
-//       }
-//       timer()
-//     }
-//
-//     function showNextSlide() {
-//       showSlide(currentSlide + 1);
-//     }
-//
-//     function showPreviousSlide() {
-//       showSlide(currentSlide - 1);
-//     }
-//
-//     function checkResult (e) {
-//       const tar = e.target;
-//       if(tar.tagName === 'INPUT') {
-//         const questionNumber = tar.name.slice(-1);
-//         const userAnswer = tar.value;
-//         const isCorrect = questions[questionNumber].correctAnswer == userAnswer;
-//         if(isCorrect) {
-//           tar.parentNode.style.color = 'limegreen';
-//         } else {
-//           tar.parentNode.style.color = 'orangered';
-//         }
-//         const radioButtons = e.currentTarget.querySelectorAll('.answers input');
-//         radioButtons.forEach(button => button.setAttribute('disabled', true));
-//       }
-//     }
-//
-//     function setAnswerHandler() {
-//       Array.from(quizContainer.querySelectorAll('.slide .answers')).forEach(answer => {
-//         answer.addEventListener('click', checkResult);
-//       })
-//     }
-//
-//     function showResults() {
-//       const answerContainers = quizContainer.querySelectorAll('.answers');
-//       let numCorrect = 0;
-//
-//       questions.forEach((currentQuestion, questionNumber) => {
-//
-//         const answerContainer = answerContainers[questionNumber];
-//         const selector = `input[name=question${questionNumber}]:checked`;
-//         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-//
-//
-//         if (userAnswer == currentQuestion.correctAnswer) {
-//           numCorrect++;
-//         }
-//       });
-//
-//       resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
-//       submitButton.style.display = 'none';
-//       restartButton.style.display = 'inline-block';
-//     }
-//
-//     function restart() {
-//       document.location.reload();
-//     }
-//
-//     showSlide(currentSlide);
-//     setAnswerHandler();
-//
-//     form.addEventListener('submit', validation);
-//     submitButton.addEventListener('click', showResults);
-//     previousButton.addEventListener("click", showPreviousSlide);
-//     nextButton.addEventListener("click", showNextSlide);
-//     restartButton.addEventListener("click", restart);
-//
-//   });
--- js/script.js
+++ js/script.js
@@ -1,98 +1,225 @@
-let num;
+let quizContainer = document.querySelector('.quiz');
+let resultsContainer = document.querySelector('.results');
+let submitButton = document.querySelector('.button_results');
+let numCorrect = 0;
+let nameForm = document.querySelector('.form');
+let sendButton = document.querySelector('.send-button');
+let userName = document.querySelector('.user-name');
+let buttonsNav = document.querySelector('.wrap-btn');
+const previousButton = document.querySelector('.button_previous');
+const nextButton = document.querySelector('.button_next');
+let slides;
+let currentSlide = 0;
+let validate = null;
+let questions;
 
-const questions = [
-    {
-        question: "Who invented JavaScript?",
-        answers: {
-            a: "Douglas Crockford",
-            b: "Sheryl Sandberg",
-            c: "Brendan Eich"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-            a: "Node.js",
-            b: "TypeScript",
-            c: "npm"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-            a: "Angular",
-            b: "jQuery",
-            c: "RequireJS",
-            d: "ESLint"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Some question",
-        answers: {
-            a: "answer1",
-            b: "answer2",
-            c: "answer3",
-            d: "answer4"
-        },
-        correctAnswer: "a"
-    },
-];
+class Quiz {
 
-let answers = ["c", "c", "d", "b"];
+    buildQuiz(items, node) {
+        const output = [];
+        items.forEach(
+          (currentQuestion, questionNumber) => {
+              let answersArray = [].concat(currentQuestion.correct_answer, currentQuestion.incorrect_answers);
+              let answersMarkup = [];
+              answersArray.sort(() => Math.random() - 0.5);
 
-// const checkAnswers = function (answersArr, questionArr) {
-//     for (let i = 0; i < questionArr.length; i++) {
-//         console.log(`Вопрос ${i+1} ${questionArr[i].question}`);
-//         if (answersArr[i] === questionArr[i].correctAnswer) {
-//             console.log(`${answersArr[i]}: ${questionArr[i].answers[answersArr[i]]} - ответ верный`)
-//         } else {
-//             console.error(`${answersArr[i]}: ${questionArr[i].answers[answersArr[i]]} - ответ неверный`);
-//             console.log(`верный ответ ${questionArr[i].correctAnswer}: ${questionArr[i].answers[questionArr[i].correctAnswer]}`)
-//         }
-//     }
-// };
+              for (let item in answersArray) {
+                  answersMarkup.push(
+                    `<label>
+                <input type='radio' name='question${questionNumber}' value='${answersArray[item]}'>
+                  ${answersArray[item]}
+                </label>`
+                  );
+              }
 
-//checkAnswers(answers, questions);
+              output.push(
+                `<div class='slide'>
+                <div class='question'> ${currentQuestion.question} </div>
+                <div class='answers'> ${answersMarkup.join(' ')} </div>
+              </div>`
+              );
+          }
+        );
+        node.innerHTML = output.join('');
+        slides = document.querySelectorAll('.slide');
+        sendButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.validateName();
+            if (validate) {
+                this.startGame();
+            }
+        });
+    }
 
-const next = document.querySelector(".next");
-const prev = document.querySelector(".previous");
-const slide = document.createElement("div");
-document.body.appendChild(slide);
-slide.innerHTML = 1;
+    validateName() {
+        let regex = /^([А-Я]{1}[а-я]{1,9}|[A-Z]{1}[a-z]{1,9})$/;
+        userName.classList.remove('error');
 
-const results = document.createElement("div");
-document.body.appendChild(results);
-
-const showResults = (answersArr, questionArr) => {
-    let score = 0; //счетчик для верных ответов
-    for (let i = 0; i < questionArr.length; i++) {
-        if (answersArr[i] === questionArr[i].correctAnswer) {
-            score++;
+        if (!regex.test(userName.value)) {
+            userName.classList.add('error');
+            let error = document.createElement('div');
+            error.className = 'error-block';
+            error.style.color = 'red';
+            error.innerHTML = 'Укажите корректное имя';
+            userName.parentElement.insertBefore(error, userName);
+            validate = false;
+        } else {
+            validate = true;
         }
     }
-    return results.innerHTML = `Количество правильных ответов: ${score}`;
-};
-showResults(answers, questions);
 
-const showNextSlide = (qualifiedName, value) => {
-    prev.disabled = false;
-    slide.innerHTML++;
-    if (slide.innerHTML >= questions.length) {
-        next.disabled = true;
+    startGame() {
+        nameForm.style.opacity = '0';
+        quizContainer.style.display = 'inline-block';
+        buttonsNav.style.display = 'flex';
+        this.showSlide(currentSlide);
     }
-};
 
-const showPrevSlide = (qualifiedName, value) => {
-    next.disabled = false;
-    slide.innerHTML--;
-    if (slide.innerHTML <= 1) {
-        prev.setAttribute("disabled", "disabled");
+    showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        console.log('currentSlide = ' + currentSlide);
+
+        if(currentSlide === 0) {
+            previousButton.style.display = 'none';
+        } else{
+            previousButton.style.display = 'block';
+        }
+
+        if(currentSlide === slides.length-1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'block';
+        } else{
+            nextButton.style.display = 'block';
+            submitButton.style.display = 'none';
+        }
+
+        submitButton.addEventListener('click', this.showResults);
+
+        this.timer();
+        this.setAnswerHandlers();
     }
-};
 
-next.addEventListener("click", showNextSlide);
-prev.addEventListener("click", showPrevSlide);
+    timer() {
+        let minutes = 0;
+        let seconds = 10;
+        let self = this;
 
+        let idInt = function () {
+            if (seconds >= 0) {
+                document.querySelector('.time').innerHTML = makeMeTwoDigits(minutes) + ":" + makeMeTwoDigits(seconds);
+                seconds--;
+            } else {
+                slides[currentSlide].classList.add('done');
+                console.log(currentSlide);
+                clearInterval(timerId);
+                console.log(currentSlide === slides.length - 1);
+                if (currentSlide === slides.length - 1) {
+                    self.showResults();
+                    return;
+                } else {
+
+                    self.showNextSlide(currentSlide);
+                }
+            }
+        };
+        let timerId = setInterval(idInt, 1000);
+
+        function makeMeTwoDigits(n) {
+            return (n < 10 ? "0" : "") + n;
+        }
+
+        nextButton.addEventListener('click', function() {
+              clearInterval(timerId);
+          }
+        );
+        previousButton.addEventListener('click', function() {
+              clearInterval(timerId);
+          }
+        );
+    }
+
+    showNextSlide(n) {
+        console.log('переключили вперед');
+        if (n < slides.length - 1) {
+            this.showSlide(n + 1);
+        }
+    }
+
+    showPreviousSlide(n) {
+        console.log('переключили назад');
+        if (n > 0) {
+            this.showSlide(n - 1);
+        }
+    }
+
+    checkResults(e) {
+        const tar = e.target;
+        if(tar.tagName === 'INPUT') {
+            const questionNumber = tar.name.slice(-1);
+            const userAnswer = tar.value;
+            const isCorrect = questions[questionNumber].correct_answer === userAnswer;
+            if(isCorrect) {
+                tar.parentNode.style.color = 'green';
+                numCorrect++;
+            } else {
+                tar.parentNode.style.color = 'red';
+            }
+            const radioButtons = e.currentTarget.querySelectorAll('.answer, input');
+            radioButtons.forEach(button => button.setAttribute('disabled', 'disable'));
+        }
+    }
+
+    setAnswerHandlers() {
+        Array.from(quizContainer.querySelectorAll('.slide .answers')).forEach(answer => {
+            answer.addEventListener('click', this.checkResults);
+        })
+    }
+
+    showResults() {
+        document.querySelector('.time').style.display = 'none';
+        quizContainer.style.display = 'none';
+        previousButton.style.display = 'none';
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'none';
+        resultsContainer.innerHTML = `${userName.value}, your result is ${numCorrect} of ${questions.length}`
+        let restart = document.querySelector('.button_restart');
+        restart.style.display = 'block';
+        restart.addEventListener('click', function() {
+            document.location.reload();
+        });
+    }
+}
+
+async function getQuestions() {
+    const response = await fetch('https://opentdb.com/api.php?amount=4&category=22&difficulty=easy&type=multiple');
+    const data = await response.json();
+    let quiz = new Quiz;
+
+    questions = data.results;
+
+    quiz.buildQuiz(questions, quizContainer);
+    nextButton.addEventListener('click', () => {
+        quiz.showNextSlide(currentSlide);
+    });
+    previousButton.addEventListener('click', () => {
+        quiz.showPreviousSlide(currentSlide);
+    });
+
+}
+
+getQuestions();
+
+// 1. Добавляем данные вопросов.
+// 2. Рисуем разметку слайдов.
+// 3. Показываем первый слайд.
+// 4. По нажатию на кнопки показываем либо предыдущий, либо следующий слайд.
+// 5. Переключаем слайды по таймеру.
+// 6. По нажатию на кнопки обнуляем таймер.
+// 7. Проверяем правильность ответов.
+// 8. Считаем правильные ответы и в конце выводим их количество.
+// 9. По кнопке обнуляем игру.
+// 10. Перед игрой спрашиваем имя игрока.
+// https://opentdb.com/api.php?amount=4&category=22&difficulty=easy&type=multiple
